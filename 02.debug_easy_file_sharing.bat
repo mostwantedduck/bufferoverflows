331bdf76f80c7200d460a1c4790e3a569733dd14
Most Wanted Duck <81170836+mostwantedduck@users.noreply.github.com>
2022-01-10 17:16:26 +0100

    Update and rename debug_easy_file_sharing.bat to 02.debug_easy_file_sharing.bat

--- 02.debug_easy_file_sharing.bat
+++ 02.debug_easy_file_sharing.bat
@@ -1,5 +1,5 @@
 runas /user:WIN10-64-VM\Daniel /savecred "taskkill /IM fsws.exe /F"
-runas /user:WIN10-64-VM\Daniel /savecred "cscript \"C:\Users\Daniel\Desktop\launch_easy_file_sharing.vbs\""
+runas /user:WIN10-64-VM\Daniel /savecred "cscript \"C:\Users\Daniel\Desktop\02.launch_easy_file_sharing.vbs\""
 timeout 2
 runas /user:WIN10-64-VM\Daniel /savecred "\"C:\Program Files (x86)\Windows Kits\10\Debuggers\x86\windbg.exe\" -g -pn fsws.exe"
 
